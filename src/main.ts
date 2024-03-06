c0098e25087558ca78a54f72f7edb996e0e980a8
wuocdat <quocdatpadpqn@gmail.com>
2024-03-06 13:45:03 +0700

    increase token experied

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.EDE_APP_PORT || 3000);
 }
 bootstrap();
